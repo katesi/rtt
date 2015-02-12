3ea6413e3d20930e31abe35fad3f72c3b2fe2d91
katesi <katesi@fb.com>
2015-02-11 18:43:21 -0800

    first commit

---
+++ rottenTomatoes-3Tests/rottenTomatoes_3Tests.m
@@ -0,0 +1,40 @@
+//
+//  rottenTomatoes_3Tests.m
+//  rottenTomatoes-3Tests
+//
+//  Created by Katerina Simonova on 2/11/15.
+//  Copyright (c) 2015 Katerina Simonova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface rottenTomatoes_3Tests : XCTestCase
+
+@end
+
+@implementation rottenTomatoes_3Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
